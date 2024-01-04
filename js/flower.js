afde2c4430f96429dc04f9591a361f4aeb619695
flower-dev-asv <flower-dev-asv@gmail.com>
2024-01-04 17:52:53 +0100

    Alex's part finished (hopefully)

--- js/flower.js
+++ js/flower.js
@@ -12,8 +12,8 @@
     }
     mybutton.addEventListener("click", backToTop);
     function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     }
 //FORM VALIDATION
     (() => {
@@ -30,5 +30,28 @@
     })
     })()
 //TOOLTIPS
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+/*RESET BUTTON*/
+    bReset = document.querySelector('[type=reset]');
+    bReset.onclick = resetColor;
+    function resetColor(){
+        const forms = document.querySelectorAll('.was-validated')
+        Array.from(forms).forEach(form => {
+            form.classList.remove('was-validated');
+        })
+    }
+/*PLACEHOLDER AND SEARCH TOOLTIP*/
+    bSearch = document.getElementById('button-search-mobile');
+    bSearch.addEventListener('click', showLoading, false);
+    function showLoading(){
+        placeholder = document.getElementsByClassName('placeholder-glow')[0];
+        placeholder.classList.remove('d-none');
+        spinner = document.getElementsByClassName('spinner-border')[0];
+        spinner.parentElement.classList.remove('d-none');
+        placeholder.onclick = function(){
+            placeholder.classList.add('d-none');
+            spinner.parentElement.classList.add('d-none');
+            bSearch.previousElementSibling.value = '';
+        };
+    }
--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 /*PAGINATION CONTROLS*/
-    pageLinks = document.getElementsByTagName('section')[1].getElementsByTagName('nav')[0].getElementsByTagName('a');
-    listItems = document.getElementsByTagName('section')[1].getElementsByTagName('nav')[0].getElementsByTagName('li');
+    pageLinks = document.getElementsByTagName('section')[0].getElementsByTagName('nav')[0].getElementsByTagName('a');
+    listItems = document.getElementsByTagName('section')[0].getElementsByTagName('nav')[0].getElementsByTagName('li');
     for (let i = 0; i < pageLinks.length; i++) {
         pageLinks[i].addEventListener('click', movePage, false);
     }
     function movePage(e){
         e.preventDefault();
         var pageID = this.href;
-        var pages = document.getElementsByTagName('section')[1].querySelectorAll('[id^=events]');
+        var pages = document.getElementsByTagName('section')[0].querySelectorAll('[id^=events]');
         for (let i = 0; i < pages.length; i++) {
             if(pageID.includes(pages[i].id)){
-                document.getElementById('events1').classList = 'row text-center d-none';
+                document.getElementById('events1').classList.add('d-none');
                 pages[i].classList.toggle('d-none');
-                pageLinks[i].parentElement.classList = 'page-item active';
+                pageLinks[i].parentElement.classList.add('active');
                 pageLinks[i].parentElement.setAttribute('aria-current', 'page');
             }else{
-                pages[i].classList = 'row text-center d-none';
-                pageLinks[i].parentElement.classList = 'page-item';
+                pages[i].classList.add('d-none');
+                pageLinks[i].parentElement.classList.remove('active');
                 pageLinks[i].parentElement.setAttribute('aria-current', 'false');
             }
         }
